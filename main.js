75ae6f431b32928b81a8f1e2102934ff0e60a831
Roger Enand <rogerenand@gmail.com>
2019-12-19 01:47:32 +0000

    changed projects from list to cards

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(initializeApp);
 var projects = null;
 function initializeApp(){
   projects = new Projects();
-  projects.filterProjects('all');
+  projects.filterProjects('JavaScript');
 }
 
 class Projects{
@@ -17,6 +17,7 @@ class Projects{
           'html5',
           'css3'
         ],
+        gitLink:'',
         link:'https://sigo.rogerenand.com'
       },
       {
@@ -28,6 +29,7 @@ class Projects{
           'css3',
           'react'
         ],
+        gitLink: 'https://github.com/enandr/open-ticket',
         link: 'https://openticket.rogerenand.com'
       },
       {
@@ -38,6 +40,7 @@ class Projects{
           'html5',
           'css3'
         ],
+        gitLink: 'https://github.com/enandr/memory-match',
         link: 'https://memorymatch.rogerenand.com'
       },
       {
@@ -49,24 +52,47 @@ class Projects{
           'css3',
           'react'
         ],
+        gitLink: 'https://github.com/enandr/wicked-sales',
         link: 'https://wickedsales.rogerenand.com'
+      },
+      {
+        name: 'Whiteboard Wizards',
+        tags: [
+          'all',
+          'javascript',
+          'html5',
+          'css3',
+          'react'
+        ],
+        gitLink: '',
+        link: ''
       }
     ]
     this.setClicks();
   }
   filterProjects(filter){
-    let newUl = $('<ul>');
+    let allCards = $('<div>').addClass('projectRow').attr('id', 'projectRow');
     this.projects.map(value => {
       if (value.tags.includes(filter.toLowerCase())){
-        let newLi = $('<li>');
-        let newA = $(`<a href="${value.link}">`);
-        newA.text(value.name);
-        newA.attr('href', value.link);
-        newLi.append(newA);
-        newUl.append(newLi);
+        let card = $('<div>').addClass('card');
+        let cardImage = $('<div>').addClass('cardImg').css({ 'background-image':"url('binary-code-475664_1920.jpg')"});
+        let title = $('<h1>').text(value.name);
+        let description = $('<p>').text('Description Goes Here');
+        let btnContainer = $('<div>').addClass('cardBtnContainer');
+        let liveBtn = $('<a>').text('Live').addClass('btn cardBtn');
+        let gitBtn = $('<a>').text('Github').addClass('btn cardBtn');
+        if (value.link){
+          liveBtn.attr('href', value.link);
+        }
+        if (value.gitLink) {
+          gitBtn.attr('href', value.gitLink);
+        }
+        btnContainer.append(liveBtn, gitBtn);
+        card.append(cardImage, title, description, btnContainer);
+        allCards.append(card);
       }
     })
-    this.show(newUl);
+    this.show(allCards);
 
   }
   setClicks(){
@@ -78,7 +104,7 @@ class Projects{
     });
   }
   show(list){
-    $('#projectRow *').remove();
-    $('#projectRow').append(list);
+    $('.projects *').remove();
+    $('.projects').append(list);
   }
 }
